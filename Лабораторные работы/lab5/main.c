b44674459a4cc4c9f37abb34524edde2c2e87954
Katya <kataklachun@mail.ru>
2026-04-23 04:33:31 +1000

    лабораторная работа №5 (операции с матрицами)

---
+++ Лабораторные работы/lab5/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "matrix.h"
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    int n, i, j;
+    char op;
+
+    printf("Введите размер матриц n: ");
+    scanf("%d", &n);
+
+    double** A = (double**)malloc(n * sizeof(double*));
+    double** B = (double**)malloc(n * sizeof(double*));
+
+    for (i = 0; i < n; i++) {
+        A[i] = (double*)malloc(n * sizeof(double));
+        B[i] = (double*)malloc(n * sizeof(double));
+    }
+
+    printf("Введите матрицу A (%dx%d):\n", n, n);
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            scanf("%lf", &A[i][j]);
+
+    printf("Введите матрицу B:\n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            scanf("%lf", &B[i][j]);
+
+    printf("Введите операцию (+, -, *): ");
+    scanf(" %c", &op);
+
+    double** result = calculate(A, B, n, op);
+
+    printf("Результат:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%.lf ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
