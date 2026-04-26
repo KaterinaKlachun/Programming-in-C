5da865a0e82be1a6f9eb0e69b3b4dd0b9fee4be2
Katya <kataklachun@mail.ru>
2026-04-26 16:50:04 +1000

    добавление заголовочного файла functions.h

---
+++ Лабораторные работы/lab11/functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+double sum_args(int count, ...);
+double max_args(int count, ...);
+double min_args(int count, ...);
+double avg_args(int count, ...);
+
+#endif
