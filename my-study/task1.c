ee7d730115c4f25b416e428c937e2c0ccd0ce60c
Katya <kataklachun@mail.ru>
2026-05-03 00:13:50 +1000

    task

---
+++ my-study/task1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <conio.h>
+
+int countBitsLong(long x)
+{
+    int count = 0;
+    unsigned long *p = (unsigned long*)&x;
+
+    for (int i = 0; i < sizeof(long) * 8; i++)
+    {
+        if (((*p) >> i) & 1)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int countBitsDouble(double x)
+{
+    int count = 0;
+    unsigned long long *p = (unsigned long long*)&x;
+
+    for (int i = 0; i < sizeof(double) * 8; i++)
+    {
+        if (((*p) >> i) & 1)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main()
+{
+    long a;
+    double b;
+
+    printf("Введите long: ");
+    scanf("%ld", &a);
+
+    printf("Введите double: ");
+    scanf("%lf", &b);
+
+    printf("Единичных битов в long: %d\n", countBitsLong(a));
+    printf("Единичных битов в double: %d\n", countBitsDouble(b));
+
+    getch();
+    return 0;
+}
