379e5d3cc52b1a4babf4d6da535ae7c3f9ea6a38
Katya <kataklachun@mail.ru>
2026-05-03 00:14:01 +1000

    task

---
+++ my-study/task2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <conio.h>
+
+void printNormal(int n)
+{
+    if (n < 10)
+    {
+        printf("%d ", n);
+        return;
+    }
+
+    printNormal(n / 10);
+    printf("%d ", n % 10);
+}
+
+void printReverse(int n)
+{
+    if (n < 10)
+    {
+        printf("%d ", n);
+        return;
+    }
+
+    printf("%d ", n % 10);
+    printReverse(n / 10);
+}
+
+int main()
+{
+    int n;
+
+    printf("Введите натуральное число: ");
+    scanf("%d", &n);
+
+    if (n <= 0)
+    {
+        printf("Ошибка: число должно быть натуральным.\n");
+        getch();
+        return 0;
+    }
+
+    printf("Обычный порядок:\n");
+    printNormal(n);
+
+    printf("\nОбратный порядок:\n");
+    printReverse(n);
+
+    getch();
+    return 0;
+}
