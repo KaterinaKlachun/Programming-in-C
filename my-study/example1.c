e30c7159ad76421b0f0d62570ec04ed9c69c4005
ekaterina <katerina.klachun@mail.ru>
2026-05-03 20:21:08 +1000

    task

---
+++ my-study/example1.c
@@ -0,0 +1,9 @@
+//Напишите программу, которая выводит на консоль две строки: "Hello World" и "Hello C"
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello World!\n");
+    printf("Hello C!\n");
+    return 0;
+}
